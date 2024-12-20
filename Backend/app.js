fe19e0c91c6bf86d2d3ec9c77c480ef6c4657047
Ankit yadav <ankityadav564682@gmail.com>
2024-12-20 02:48:46 -0800

    basic setup

---
+++ Backend/app.js
@@ -0,0 +1,13 @@
+const dotenv = require('dotenv');
+dotenv.config({path: "/.env"});
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+app.get("/",(req,res)=>{
+    res.send("Welcome to my API!");
+})
+
+module.exports =app;
